acb9259f6fba502279c38bcaa00500871451de1c
Viktoriia Serkiz <v.serkiz@gmail.com>
2023-11-28 21:45:24 +0200

    added text

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 }
@@ -13,8 +11,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2dcbd",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#121cf4",
+    fontSize: 30,
+    fontWeight: "700",
   },
 });
