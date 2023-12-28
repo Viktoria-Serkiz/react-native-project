509d33d89503587fc072601bf4aab67c6c4fd8e7
Viktoriia Serkiz <v.serkiz@gmail.com>
2023-12-28 08:39:44 +0200

    fixed backet screen,confirm button clean orders

--- src/screens/home/screens/BasketScreen.js
+++ src/screens/home/screens/BasketScreen.js
@@ -3,16 +3,20 @@ import { SafeAreaView, TouchableOpacity, Text, FlatList } from "react-native";
 import { observer } from "mobx-react";
 import orderStore from "../store/Order";
 
-export const BasketScreen = observer(() => {
+export const BasketScreen = observer(({ navigation }) => {
   const navigateBack = () => {
     navigation.goBack();
   };
 
   const renderItem = ({ item }) => (
     <TouchableOpacity onPress={() => orderStore.removeOrder(item)}>
-      <Text>{item.title}</Text>
-      <Text>{`Price: ${item.price}`}</Text>
-      <Text>Remove</Text>
+      {item && item.title && (
+        <>
+          <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
+          <Text style={{ fontWeight: "bold" }}>{`Price: ${item.price}`}</Text>
+          <Text>Remove</Text>
+        </>
+      )}
     </TouchableOpacity>
   );
 
@@ -20,128 +24,31 @@ export const BasketScreen = observer(() => {
     orderStore.confirmOrder();
   };
 
+  console.log("orderStore.orders:", orderStore.orders);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, padding: 16 }}>
       <TouchableOpacity onPress={navigateBack}>
-        <Text>Basket Screen</Text>
-        <Text>{`In your shopping cart ${orderStore.orders.length} items`}</Text>
+        <Text style={{ fontSize: 20, marginBottom: 10 }}>Basket Screen</Text>
       </TouchableOpacity>
 
+      <Text
+        style={{ fontSize: 20 }}
+      >{`In your shopping cart ${orderStore.orders.length} items`}</Text>
       <FlatList
         data={orderStore.orders}
         keyExtractor={(item, index) => index + ""}
         renderItem={renderItem}
       />
 
-      <TouchableOpacity onPress={confirmOrder}>
-        <Text>Confirm Order</Text>
+      <TouchableOpacity
+        onPress={confirmOrder}
+        style={{ backgroundColor: "blue", padding: 10, marginTop: 20 }}
+      >
+        <Text style={{ color: "white", textAlign: "center" }}>
+          Confirm Order
+        </Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 });
-
-// import { SafeAreaView, TouchableOpacity, Text } from "react-native";
-// import { useNavigation, useRoute } from "@react-navigation/native";
-// import orderStore from "../store/Order";
-
-// export const BasketScreen = () => {
-//   const navigation = useNavigation();
-//   const route = useRoute();
-
-//   const navigateBack = () => {
-//     navigation.goBack();
-//   };
-
-//   return (
-//     <SafeAreaView style={{ flex: 1 }}>
-//       <TouchableOpacity onPress={navigateBack}>
-//         <Text>Basket Screen</Text>
-//         <Text>{`In your shopping cart ${orderStore.orders.length}`}</Text>
-//       </TouchableOpacity>
-//     </SafeAreaView>
-//   );
-// };
-
-// import React from "react";
-// import {
-//   SafeAreaView,
-//   TouchableOpacity,
-//   FlatList,
-//   View,
-//   Text,
-// } from "react-native";
-// import { useNavigation } from "@react-navigation/native";
-// import orderStore from "../store/Order";
-
-// export const BasketScreen = ({ route }) => {
-//   const navigation = useNavigation();
-
-//   const navigateBack = () => {
-//     navigation.goBack();
-//   };
-
-//   const removeItem = (itemId) => {
-//     orderStore.removeOrder(itemId);
-//   };
-
-//   const getTotalPrice = () => {
-//     return orderStore.orders.reduce((total, item) => total + item.price, 0);
-//   };
-
-//   const renderItem = ({ item }) => (
-//     <View
-//       style={{
-//         flexDirection: "row",
-//         justifyContent: "space-between",
-//         marginBottom: 10,
-//       }}
-//     >
-//       <Text>{pizza.name}</Text>
-//       <TouchableOpacity onPress={() => removeItem(item.id)}>
-//         <Text style={{ color: "red" }}>Remove</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-
-//   const confirmOrder = () => {};
-
-//   return (
-//     <SafeAreaView style={{ flex: 1, padding: 16 }}>
-//       <TouchableOpacity onPress={navigateBack}>
-//         <Text style={{ fontSize: 20, marginBottom: 10 }}>Basket Screen</Text>
-//         <Text>{`In your shopping cart: ${orderStore.orders.length} items`}</Text>
-//       </TouchableOpacity>
-
-//       <FlatList
-//         data={orderStore.orders}
-//         renderItem={renderItem}
-//         keyExtractor={(item) => item.id.toString()}
-//         style={{ marginTop: 20 }}
-//       />
-
-//       <View
-//         style={{
-//           flexDirection: "row",
-//           justifyContent: "space-between",
-//           marginTop: 20,
-//         }}
-//       >
-//         <Text style={{ fontWeight: "bold" }}>{`Total:`}</Text>
-//         <Text style={{ fontWeight: "bold" }}>{`$${getTotalPrice().toFixed(
-//           2
-//         )}`}</Text>
-//       </View>
-
-//       <TouchableOpacity
-//         onPress={confirmOrder}
-//         style={{ backgroundColor: "blue", padding: 10, marginTop: 20 }}
-//       >
-//         <Text style={{ color: "white", textAlign: "center" }}>
-//           Confirm Order
-//         </Text>
-//       </TouchableOpacity>
-//     </SafeAreaView>
-//   );
-// };
-
-// BasketScreen.js
